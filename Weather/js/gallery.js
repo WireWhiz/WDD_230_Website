1b8501c7f1721afce615076e25b98ef7aaba9fbb
ELIS-MOBILE-BAT\eli <eli@knisley.info>
2021-06-30 20:58:51 -0600

    made images append to gallery div

--- Weather/js/gallery.js
+++ Weather/js/gallery.js
@@ -7,6 +7,15 @@ document.addEventListener("DOMContentLoaded", function(e){
            images = request.responseText.split("\n");
            
             console.log(images);
+
+            var gallery = document.getElementsByClassName("gallery")[0];
+            images.forEach(image => {
+                var imageElement = document.createElement("img");
+                imageElement.setAttribute("src", image);
+                imageElement.setAttribute("class", "gallery_img");
+                gallery.appendChild(newImage);
+            });
+            
         }
     };
     request.open("GET", "images/gallery/img_list.txt", true);
